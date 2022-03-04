94916b400accc868d679fe05c2aa13a0a143dfc3
Ông Triệu Hậu <58720300+o-ngtrieuhau861gmailcom@users.noreply.github.com>
2022-03-04 15:24:29 +0700

    Init Repo

---
+++ dist/ojsonserver.ps1
@@ -0,0 +1,54 @@
+$ErrorActionPreference = 'Stop'
+[Console]::OutputEncoding = [Text.UTF8Encoding]::UTF8
+$PSDefaultParameterValues['*:Encoding'] = 'utf8'
+$PSDefaultParameterValues['Out-File:Encoding'] = 'utf8'
+Function InitializeExecuter {
+    $options = [pscustomobject]@{}
+    $optionsPath = Join-Path -Path $PSScriptRoot -ChildPath '\option.executer.json'
+    if ([System.IO.File]::Exists($optionsPath )) {  
+        $options = (Get-Content $optionsPath | ConvertFrom-Json)
+    }
+    $options
+}
+Function InitializeSecrets([pscustomobject] $optionsExecuter) {
+    if ($options.IsShowLogInitializeSecrets -eq $True) {
+        Write-Host '=====InitializeSecrets====='
+    }
+    $secrets = [pscustomobject]@{}
+    try {
+        $GITHUB_secrets = $env:GITHUB_secrets | ConvertFrom-Json
+        $GITHUB_secrets.PSObject.Properties | ForEach-Object {
+            if ($_.Name -ne "github_token") {
+                $converFromBase64 = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($_.Value))
+                $secrets | Add-Member `
+                    -NotePropertyName $_.Name `
+                    -NotePropertyValue ($converFromBase64 | ConvertFrom-Json)   
+            }  
+            else {
+                $secrets | Add-Member `
+                    -NotePropertyName $_.Name `
+                    -NotePropertyValue $_.Value 
+            }
+        }
+    }
+    catch {
+        $secretsPath = Join-Path -Path $PSScriptRoot -ChildPath '.githubsecrets'
+        if ([System.IO.Directory]::Exists($secretsPath )) {            
+            $secretsPath = Join-Path -Path $secretsPath -ChildPath '*'
+            $extension = ".githubsecrets.json"
+            $table = get-childitem -Path ($secretsPath) -Include @('*' + $extension)
+            foreach ($file in $table) {
+                $secrets | Add-Member `
+                    -NotePropertyName ($file.Name.Replace($extension, '')) `
+                    -NotePropertyValue (Get-Content $file.Fullname | ConvertFrom-Json)
+            }
+        }
+    }
+    if ($options.IsShowLogInitializeSecrets -eq $True) {
+        Write-Host ($secrets | ConvertTo-Json)
+        Write-Host '=====END:InitializeSecrets='
+    }
+    $secrets
+}
+$options = InitializeExecuter
+$secrets = InitializeSecrets -optionsExecuter $options
---
+++ source/ojsonserver.js
@@ -0,0 +1,47 @@
+const fs = require("fs");
+const path = require("path");
+
+const InitializeExecuter = () => {
+   let options = {};
+   let optionsPath = path.join(path.dirname(__filename), "option.executer.json");
+   if (fs.existsSync(optionsPath)) {
+      options = JSON.parse(fs.readFileSync(optionsPath, { encoding: "utf-8" }));
+   }
+   return options;
+};
+const InitializeSecrets = (options) => {
+   const isShowLog = options?.IsShowLogInitializeSecrets || false;
+   if (isShowLog) {
+      console.log("=====InitializeSecrets=====");
+   }
+   let secrets = {};
+   if ("context_github" in process.env) {
+      secrets.context_github = JSON.parse(process.env.context_github);
+   }
+   if ("GITHUB_secrets" in process.env) {
+      const GITHUB_secrets = JSON.parse(process.env.GITHUB_secrets);
+      for (const [key, value] of Object.entries(GITHUB_secrets)) {
+         if (key !== "github_token") secrets[key] = JSON.parse(Buffer.from(value, "base64").toString("utf8"));
+      }
+   } else {
+      let secretsPath = path.join(path.dirname(__filename), ".githubsecrets");
+      if (fs.existsSync(secretsPath)) {
+         let files = fs.readdirSync(secretsPath);
+         let extension = ".githubsecrets.json";
+         for (let i = 0; i < files.length; i++) {
+            const file = files[i];
+            if (file.endsWith(extension)) {
+               let contentFile = fs.readFileSync(path.join(secretsPath, file), { encoding: "utf8" });
+               secrets[file.replace(extension, "")] = JSON.parse(contentFile);
+            }
+         }
+      }
+   }
+   if (isShowLog) {
+      console.log(JSON.stringify(secrets, null, "\t"));
+      console.log("=====END:InitializeSecrets=");
+   }
+   return secrets;
+};
+const options = InitializeExecuter();
+const secrets = InitializeSecrets(options);
