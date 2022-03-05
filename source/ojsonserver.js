a828301a86c68b27e2910c860c6769126771dc18
ongtrieuhau861322 <ongtrieuhau861.322@gmail.com>
2022-03-05 12:30:00 +0700

    Test run on github

--- source/ojsonserver.js
+++ source/ojsonserver.js
@@ -1,6 +1,14 @@
 const fs = require("fs");
 const path = require("path");
 
+const JwtToken = require("E:/CLOUDCODE/Github/oListRepos/NodeJS/oModules/googleAuth/jwt.js").JwtToken;
+const oAxios = require("E:/CLOUDCODE/Github/oListRepos/NodeJS/oModules/oAxios.js");
+const oUtils = require("E:/CLOUDCODE/Github/oListRepos/NodeJS/oModules/oUtils.js");
+const oCrytoJS = require("E:/CLOUDCODE/Github/oListRepos/NodeJS/oModules/oCrytoJS.js");
+
+let scopes = "https://www.googleapis.com/auth/firebase.database https://www.googleapis.com/auth/userinfo.email";
+const metaOnHostFields = "a0metaonhost";
+
 const InitializeExecuter = () => {
    let options = {};
    let optionsPath = path.join(path.dirname(__filename), "option.executer.json");
@@ -37,6 +45,7 @@ const InitializeSecrets = (options) => {
          }
       }
    }
+   secrets.CONFIG = oUtils.MapFieldsConfig(secrets.CONFIG);
    if (isShowLog) {
       console.log(JSON.stringify(secrets, null, "\t"));
       console.log("=====END:InitializeSecrets=");
@@ -45,4 +54,63 @@ const InitializeSecrets = (options) => {
 };
 const options = InitializeExecuter();
 const secrets = InitializeSecrets(options);
-console.log(options);
+
+(async () => {
+   //Get Files
+   let dataPath = path.join(path.dirname(__filename), "..", options.DataDirectoryName);
+   let files = oUtils.GetAllFiles(dataPath, []);
+   files = files.filter((e) => e.toString().endsWith(options.DataExtensionFile)).sort();
+   files = files.map((file) => {
+      let result = {
+         local_path: file,
+         host_path: oUtils.oPath.ToHostPath(file.replace(dataPath, "")).replace(options.DataExtensionFile, ""),
+         content_md5: "",
+         content_json: oUtils.JSONLoadForce(file),
+      };
+      result.content_md5 = oCrytoJS.HashMD5Object({ content_json: result.content_json, host_path: result.host_path });
+      return result;
+   });
+   let metaTo = files.reduce((total, file) => {
+      total[file.content_md5] = file.host_path;
+      return total;
+   }, {});
+   metaTo.content_md5 = oCrytoJS.HashMD5Object(metaTo);
+   if (files.length === 0) return;
+
+   /**
+    * !Transfer data to rtdbs
+    */
+   let rtdbs = secrets.CONFIG.rtdbs || [];
+   for (let i = 0; i < rtdbs.length; i++) {
+      let rtdb = rtdbs[i];
+      rtdb.access_token = (await JwtToken(rtdb.client_email, scopes, rtdb.private_key)).access_token;
+      rtdb.rtdb_url = oUtils.oString.TrimEnd(rtdb.rtdb_url, "/");
+      rtdb.metaonhost_url = `${rtdb.rtdb_url}/${metaOnHostFields}.json`;
+      rtdb.axios_config = { url: rtdb.metaonhost_url, access_token: rtdb.access_token };
+      await oAxios.Get(rtdb.axios_config).then((data) => {
+         let metaOn = oUtils.ToObjectForce(data);
+         if (!(metaTo.content_md5 in metaOn)) {
+            let allPromises = files
+               .filter((file) => !(file.content_md5 in metaOn))
+               .map((file) => {
+                  return oAxios.Put({ url: `${rtdb.rtdb_url}/${file.host_path}.json`, access_token: rtdb.access_token, data: file.content_json });
+               });
+            Object.keys(metaOn).map((key) => {
+               if (key !== "content_md5" && !(key in metaTo)) {
+                  allPromises.push(oAxios.Delete({ url: `${rtdb.rtdb_url}/${metaOn[key]}.json`, access_token: rtdb.access_token }));
+               }
+            });
+            if (allPromises && allPromises.length > 0) {
+               Promise.allSettled(allPromises).then((values) => {
+                  if (values.length > 0 && values.findIndex((value) => value.status !== "fulfilled") === -1) {
+                     oAxios.Put({ url: rtdb.metaonhost_url, access_token: rtdb.access_token, data: metaTo }).then((data) => {
+                        console.log(`OK:${metaOnHostFields}:${rtdb.metaonhost_url}`);
+                     });
+                  }
+               });
+            }
+         }
+      });
+   }
+   return;
+})();
